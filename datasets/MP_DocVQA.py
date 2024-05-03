ee655c66efbefbc4790be04ecdd76042864917fd
Zhong <winnieyizhong@gmail.com>
2024-05-03 16:02:17 -0500

    included context in MP_DocVQA

--- datasets/MP_DocVQA.py
+++ datasets/MP_DocVQA.py
@@ -54,113 +54,113 @@ class MPDocVQA(Dataset):
         # answer_page_idx = record['answer_page_idx']
         num_pages = 1
 
-        # if self.page_retrieval == 'oracle':
-        #     context = ' '.join([word.lower() for word in record['ocr_tokens'][answer_page_idx]])
-        #     context_page_corresp = None
-        #     num_pages = 1
+        if self.page_retrieval == 'oracle':
+            context = ' '.join([word.lower() for word in record['ocr_tokens'][answer_page_idx]])
+            context_page_corresp = None
+            num_pages = 1
 
-        #     if self.use_images:
-        #         image_names = os.path.join(self.images_dir, "{:s}.jpg".format(record['image_name'][answer_page_idx]))
-        #         images = Image.open(image_names).convert("RGB")
+            if self.use_images:
+                image_names = os.path.join(self.images_dir, "{:s}.jpg".format(record['image_name'][answer_page_idx]))
+                images = Image.open(image_names).convert("RGB")
 
-        #     if self.get_raw_ocr_data:
-        #         words = [word.lower() for word in record['ocr_tokens'][answer_page_idx]]
-        #         boxes = np.array([bbox for bbox in record['ocr_normalized_boxes'][answer_page_idx]])
+            if self.get_raw_ocr_data:
+                words = [word.lower() for word in record['ocr_tokens'][answer_page_idx]]
+                boxes = np.array([bbox for bbox in record['ocr_normalized_boxes'][answer_page_idx]])
 
-        # elif self.page_retrieval == 'concat':
-        #     context = ""
-        #     context_page_corresp = []
-        #     for page_ix in range(record['imdb_doc_pages']):
-        #         page_context = " ".join([word.lower() for word in record['ocr_tokens'][page_ix]])
-        #         context += " " + page_context
-        #         context_page_corresp.extend([-1] + [page_ix]*len(page_context))
+        elif self.page_retrieval == 'concat':
+            context = ""
+            context_page_corresp = []
+            for page_ix in range(record['imdb_doc_pages']):
+                page_context = " ".join([word.lower() for word in record['ocr_tokens'][page_ix]])
+                context += " " + page_context
+                context_page_corresp.extend([-1] + [page_ix]*len(page_context))
 
-        #     context = context.strip()
-        #     context_page_corresp = context_page_corresp[1:]  # Remove the first character corresponding to the first space.
+            context = context.strip()
+            context_page_corresp = context_page_corresp[1:]  # Remove the first character corresponding to the first space.
 
-        #     if self.get_raw_ocr_data:
-        #         words = []
-        #         for p in range(num_pages):
-        #             words.extend([word.lower() for word in record['ocr_tokens'][p]])
+            if self.get_raw_ocr_data:
+                words = []
+                for p in range(num_pages):
+                    words.extend([word.lower() for word in record['ocr_tokens'][p]])
 
-        #             """
-        #             mod_boxes = record['ocr_normalized_boxes'][p]
-        #             mod_boxes[:, 1] = mod_boxes[:, 1]/num_pages + p/num_pages
-        #             mod_boxes[:, 3] = mod_boxes[:, 3]/num_pages + p/num_pages
+                    """
+                    mod_boxes = record['ocr_normalized_boxes'][p]
+                    mod_boxes[:, 1] = mod_boxes[:, 1]/num_pages + p/num_pages
+                    mod_boxes[:, 3] = mod_boxes[:, 3]/num_pages + p/num_pages
 
-        #             boxes.extend(mod_boxes)  # bbox in l,t,r,b
-        #             """
-        #         # boxes = np.array(boxes)
-        #         boxes = record['ocr_normalized_boxes']
+                    boxes.extend(mod_boxes)  # bbox in l,t,r,b
+                    """
+                # boxes = np.array(boxes)
+                boxes = record['ocr_normalized_boxes']
 
-        #     else:
-        #         words, boxes = None, None
+            else:
+                words, boxes = None, None
 
-        #     if self.use_images:
-        #         image_names = [os.path.join(self.images_dir, "{:s}.jpg".format(image_name)) for image_name in record['image_name']]
-        #         images = [Image.open(img_path).convert("RGB") for img_path in image_names]
-        #         images, boxes = utils.create_grid_image(images, boxes)
+            if self.use_images:
+                image_names = [os.path.join(self.images_dir, "{:s}.jpg".format(image_name)) for image_name in record['image_name']]
+                images = [Image.open(img_path).convert("RGB") for img_path in image_names]
+                images, boxes = utils.create_grid_image(images, boxes)
 
-        #     else:
-        #         boxes = np.array(boxes)
+            else:
+                boxes = np.array(boxes)
 
-        # elif self.page_retrieval == 'logits':
-        #     context = []
-        #     for page_ix in range(record['imdb_doc_pages']):
-        #         context.append(' '.join([word.lower() for word in record['ocr_tokens'][page_ix]]))
+        elif self.page_retrieval == 'logits':
+            context = []
+            for page_ix in range(record['imdb_doc_pages']):
+                context.append(' '.join([word.lower() for word in record['ocr_tokens'][page_ix]]))
 
-        #     context_page_corresp = None
+            context_page_corresp = None
 
-        #     if self.use_images:
-        #         image_names = [os.path.join(self.images_dir, "{:s}.jpg".format(image_name)) for image_name in record['image_name']]
-        #         images = [Image.open(img_path).convert("RGB") for img_path in image_names]
+            if self.use_images:
+                image_names = [os.path.join(self.images_dir, "{:s}.jpg".format(image_name)) for image_name in record['image_name']]
+                images = [Image.open(img_path).convert("RGB") for img_path in image_names]
 
-        #     if self.get_raw_ocr_data:
-        #         words = []
-        #         boxes = record['ocr_normalized_boxes']
-        #         for p in range(num_pages):
-        #             words.append([word.lower() for word in record['ocr_tokens'][p]])
+            if self.get_raw_ocr_data:
+                words = []
+                boxes = record['ocr_normalized_boxes']
+                for p in range(num_pages):
+                    words.append([word.lower() for word in record['ocr_tokens'][p]])
 
-        # elif self.page_retrieval == 'custom':
-        #     first_page, last_page = self.get_pages(record)
-        #     answer_page_idx = answer_page_idx - first_page
-        #     num_pages = len(range(first_page, last_page))
+        elif self.page_retrieval == 'custom':
+            first_page, last_page = self.get_pages(record)
+            answer_page_idx = answer_page_idx - first_page
+            num_pages = len(range(first_page, last_page))
 
-        #     words = []
-        #     boxes = []
-        #     context = []
-        #     image_names = []
+            words = []
+            boxes = []
+            context = []
+            image_names = []
 
-        #     for page_ix in range(first_page, last_page):
-        #         words.append([word.lower() for word in record['ocr_tokens'][page_ix]])
-        #         boxes.append(np.array(record['ocr_normalized_boxes'][page_ix], dtype=np.float32))
-        #         context.append(' '.join([word.lower() for word in record['ocr_tokens'][page_ix]]))
-        #         image_names.append(os.path.join(self.images_dir, "{:s}.jpg".format(record['image_name'][page_ix])))
+            for page_ix in range(first_page, last_page):
+                words.append([word.lower() for word in record['ocr_tokens'][page_ix]])
+                boxes.append(np.array(record['ocr_normalized_boxes'][page_ix], dtype=np.float32))
+                context.append(' '.join([word.lower() for word in record['ocr_tokens'][page_ix]]))
+                image_names.append(os.path.join(self.images_dir, "{:s}.jpg".format(record['image_name'][page_ix])))
 
-        #     context_page_corresp = None
+            context_page_corresp = None
 
-        #     if num_pages < self.max_pages:
-        #         for _ in range(self.max_pages - num_pages):
-        #             words.append([''])
-        #             boxes.append(np.zeros([1, 4], dtype=np.float32))
+            if num_pages < self.max_pages:
+                for _ in range(self.max_pages - num_pages):
+                    words.append([''])
+                    boxes.append(np.zeros([1, 4], dtype=np.float32))
 
-        #     if self.use_images:
-        #         images = [Image.open(img_path).convert("RGB") for img_path in image_names]
-        #         images += [Image.new('RGB', (2, 2)) for i in range(self.max_pages - len(image_names))]  # Pad with 2x2 images.
+            if self.use_images:
+                images = [Image.open(img_path).convert("RGB") for img_path in image_names]
+                images += [Image.new('RGB', (2, 2)) for i in range(self.max_pages - len(image_names))]  # Pad with 2x2 images.
 
-        # if self.page_retrieval in ['oracle', 'concat', 'none']:
-        #     start_idxs, end_idxs = self._get_start_end_idx(context, answers)
+        if self.page_retrieval in ['oracle', 'concat', 'none']:
+            start_idxs, end_idxs = self._get_start_end_idx(context, answers)
 
-        # elif self.page_retrieval == 'logits':
-        #     start_idxs, end_idxs = self._get_start_end_idx(context[answer_page_idx], answers)
+        elif self.page_retrieval == 'logits':
+            start_idxs, end_idxs = self._get_start_end_idx(context[answer_page_idx], answers)
 
-        # else:
-        #     start_idxs, end_idxs = None, None
+        else:
+            start_idxs, end_idxs = None, None
 
         sample_info = {'question_id': record['question_id'],
                        'questions': question,
-                    #    'contexts': context,
-                    #    'context_page_corresp': context_page_corresp,
+                       'contexts': context,
+                       'context_page_corresp': context_page_corresp,
                        'answers': answers,
                     #    'answer_page_idx': answer_page_idx,
                        'num_pages': num_pages
